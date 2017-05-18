04e2d44de43650ec85fcd717bc9c19f33d57f137
Jochen Berger <fooberger@gmail.com>
2017-05-18 09:39:21 +0200

    use marionette

--- src/test/groovy/GebConfig.groovy
+++ src/test/groovy/GebConfig.groovy
@@ -1,11 +1,10 @@
 import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.remote.DesiredCapabilities
+
+import io.github.bonigarcia.wdm.FirefoxDriverManager
 
 reportsDir = 'build/reports/geb'
 baseUrl = "http://localhost:${System.properties['jettyPort']}/"
 driver = {
-  DesiredCapabilities.firefox().with {
-    setCapability("marionette", false);
-    new FirefoxDriver(it)
-  }
+  FirefoxDriverManager.getInstance().setup()
+  new FirefoxDriver()
 }
