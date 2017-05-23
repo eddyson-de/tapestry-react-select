684ba03e1c513fee0b9d7fd6a636283018cbde1d
Jochen Berger <fooberger@gmail.com>
2017-05-23 15:12:50 +0200

    use the sledgehammer

--- rollup/react-select.js
+++ rollup/react-select.js
@@ -10,6 +10,6 @@ function defaultChildren (props) {
 // work around https://github.com/rollup/rollup-plugin-commonjs/issues/105,
 // https://github.com/JedWatson/react-select/issues/1517,
 // https://github.com/JedWatson/react-select/pull/1741
-Select.Async.defaultProps = Object.assign({}, Select.Async.defaultProps, {children: defaultChildren}); 
+Select.Async.defaultProps.children = defaultChildren; 
 
 export default Select;
