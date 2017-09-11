340cbf42fa586747e065422d389193a484aa378b
Jochen Berger <fooberger@gmail.com>
2017-09-11 09:46:19 +0200

    remove workaround

--- rollup/react-select.js
+++ rollup/react-select.js
@@ -1,15 +1,3 @@
 import Select from 'react-select';
-import React from 'react';
-
-function defaultChildren (props) {
-  return (
-      <Select {...props} />
-  );
-};
-
-// work around https://github.com/rollup/rollup-plugin-commonjs/issues/105,
-// https://github.com/JedWatson/react-select/issues/1517,
-// https://github.com/JedWatson/react-select/pull/1741
-Select.Async.defaultProps.children = defaultChildren; 
 
 export default Select;
